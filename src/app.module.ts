37093be4b13797192732afa8b3c0dcc59a89cf6c
Pedro Marques <pedro.marques@ufpi.edu.br>
2023-12-23 15:44:51 -0300

    add user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './domain/users/users.module';
 
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot()],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/infra/database/schemas/user.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, now } from 'mongoose';
+
+export type CatDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ default: now() })
+  createdAt: Date;
+
+  @Prop({ default: now() })
+  updatedAt: Date;
+
+  @Prop()
+  deletedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
