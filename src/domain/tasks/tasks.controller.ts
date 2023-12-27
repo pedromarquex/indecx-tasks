d032492420fcd00f29241f9c2973b8fe0ce9e93c
Pedro Marques <pedro.marques@ufpi.edu.br>
2023-12-27 15:53:07 -0300

    Add Swagger API documentation to tasks controller

--- src/domain/tasks/tasks.controller.ts
+++ src/domain/tasks/tasks.controller.ts
@@ -11,7 +11,16 @@ import {
   Request,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { AuthGuard } from '../users/auth.guard';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
@@ -22,7 +31,27 @@ import { TasksService } from './tasks.service';
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
-  @ApiOperation({ summary: 'Create a new task' })
+  @ApiOperation({
+    summary: 'Create a new task',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/components/schemas/CreateTaskDto',
+          },
+        },
+      },
+    },
+  })
+  @ApiCreatedResponse({
+    description: 'The record has been successfully created.',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized.',
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request.',
+  })
   @ApiBearerAuth()
   @UseGuards(AuthGuard)
   @Post()
@@ -32,6 +61,12 @@ export class TasksController {
   }
 
   @ApiOperation({ summary: 'Get all tasks' })
+  @ApiOkResponse({
+    description: 'The records have been successfully retrieved.',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized.',
+  })
   @ApiBearerAuth()
   @UseGuards(AuthGuard)
   @Get()
@@ -41,6 +76,15 @@ export class TasksController {
   }
 
   @ApiOperation({ summary: 'Get a task' })
+  @ApiOkResponse({
+    description: 'The record has been successfully retrieved.',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized.',
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request.',
+  })
   @ApiBearerAuth()
   @UseGuards(AuthGuard)
   @Get(':id')
@@ -49,7 +93,27 @@ export class TasksController {
     return this.tasksService.findOne(id, request['userId']);
   }
 
-  @ApiOperation({ summary: 'Update a task' })
+  @ApiOperation({
+    summary: 'Update a task',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/components/schemas/UpdateTaskDto',
+          },
+        },
+      },
+    },
+  })
+  @ApiOkResponse({
+    description: 'The record has been successfully updated.',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized.',
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request.',
+  })
   @ApiBearerAuth()
   @UseGuards(AuthGuard)
   @Patch(':id')
@@ -63,6 +127,15 @@ export class TasksController {
   }
 
   @ApiOperation({ summary: 'Delete a task' })
+  @ApiNoContentResponse({
+    description: 'The record has been successfully deleted.',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized.',
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request.',
+  })
   @ApiBearerAuth()
   @UseGuards(AuthGuard)
   @Delete(':id')
